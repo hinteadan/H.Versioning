192e7bd0990a10e39384659b71cba9ee5f520f52
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2016-11-27 17:13:22 +0200

    Define version model

---
+++ H.Versioning/H.Versioning/Version.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace H.Versioning
+{
+    public sealed class Version
+    {
+        public static readonly Version Unknown = new Version(VersionNumber.Unknown, DateTime.MinValue, "N/A", "N/A");
+
+        private static readonly string[] possibleVersionPartsSeparators = new string[] { "\r", "\n", "\t", "; ", ", ", "| " };
+
+        public readonly VersionNumber Number;
+        public readonly DateTime Timestamp;
+        public readonly string Branch;
+        public readonly string Commit;
+
+        public Version(VersionNumber number, DateTime timestamp, string branch, string commit)
+        {
+            this.Number = number;
+            this.Timestamp = timestamp;
+            this.Branch = branch;
+            this.Commit = commit;
+        }
+
+        public override string ToString()
+        {
+            return ToString(Environment.NewLine);
+        }
+
+        public string ToString(string separator)
+        {
+            return $"{Number}{separator}{Timestamp.ToString(CultureInfo.InvariantCulture)}{separator}{Branch}{separator}{Commit}";
+        }
+
+        public static Version Parse(string versionString)
+        {
+            if (string.IsNullOrWhiteSpace(versionString) || !versionString.Contains("."))
+            {
+                throw new InvalidOperationException("The given version string does not have the expected format");
+            }
+
+            var parts = versionString.Split(possibleVersionPartsSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            var number = VersionNumber.Parse(parts[0].Trim());
+            var timestamp = DateTime.MinValue;
+            string branch = parts.Length > 2 ? parts[2].Trim() : null;
+            string commit = parts.Length > 3 ? parts[3].Trim() : null;
+            if (parts.Length > 1)
+            {
+                DateTime.TryParse(parts[1].Trim(), CultureInfo.InvariantCulture, DateTimeStyles.None, out timestamp);
+            }
+
+            return new Version(number, timestamp, branch, commit);
+        }
+    }
+}
---
+++ H.Versioning/H.Versioning/VersionNumber.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace H.Versioning
+{
+    public sealed class VersionNumber
+    {
+        public static readonly VersionNumber Unknown = new VersionNumber(0, 0, 0, 0, "unknown");
+
+        public readonly int Major;
+        public readonly int Minor;
+        public readonly int? Patch;
+        public readonly int? Build;
+        public readonly string Suffix;
+
+        public VersionNumber(int major, int minor, int? patch, int? build, string suffix)
+        {
+            this.Major = major;
+            this.Minor = minor;
+            this.Patch = patch;
+            this.Build = build;
+            this.Suffix = suffix;
+        }
+        public VersionNumber(int major, int minor) : this(major, minor, null, null, null) { }
+        public VersionNumber(int major, int minor, int patch) : this(major, minor, patch, null, null) { }
+        public VersionNumber(int major, int minor, int patch, int build) : this(major, minor, patch, build, null) { }
+
+        public static VersionNumber Parse(string semanticVersion)
+        {
+            if (string.IsNullOrWhiteSpace(semanticVersion) || !semanticVersion.Contains("."))
+            {
+                throw new InvalidOperationException("The given version is not in semantic format");
+            }
+
+            var versionAndSuffixParts = semanticVersion.Split(new char[] { '-' }, 2, StringSplitOptions.RemoveEmptyEntries);
+            var versionString = versionAndSuffixParts[0].ToLowerInvariant().Replace("v", string.Empty).Trim();
+            string suffix = versionAndSuffixParts.Length > 1 ? versionAndSuffixParts[1].Trim() : null;
+
+            var versionParts = versionString.Split(new char[] { '.' }, 4, StringSplitOptions.RemoveEmptyEntries);
+            var major = int.Parse(versionParts[0].Trim());
+            var minor = int.Parse(versionParts[1].Trim());
+            var patch = versionParts.Length > 2 ? (int?)int.Parse(versionParts[2].Trim()) : null;
+            var build = versionParts.Length > 3 ? (int?)int.Parse(versionParts[3].Trim()) : null;
+
+            return new VersionNumber(major, minor, patch, build, suffix);
+        }
+
+        public override string ToString()
+        {
+            var versionString = new StringBuilder($"{this.Major}.{this.Minor}");
+
+            if (this.Patch != null)
+            {
+                versionString.Append($".{this.Patch}");
+
+                if (this.Build != null)
+                {
+                    versionString.Append($".{this.Build}");
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.Suffix))
+            {
+                versionString.Append($"-{this.Suffix}");
+            }
+
+            return versionString.ToString();
+        }
+    }
+}
