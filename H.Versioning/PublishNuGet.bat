b11a0a8d4a63ad6dbdbcc5ff6a86d90b9305d9aa
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2016-11-30 01:52:11 +0200

    OD: Automate nuget versioning

--- H.Versioning/PublishNuGet.bat
+++ H.Versioning/PublishNuGet.bat
@@ -1,6 +1,10 @@
 call .\Release.bat
 
+call .\Build\Publish\H.Versioning.Cli.exe nuspec "H.Versioning.Cli\H.Versioning.Cli.nuspec"
+
 del /S /Q .\*nupkg
 
 .\nuget.exe pack ".\H.Versioning.Cli\H.Versioning.Cli.csproj" -Prop Configuration=Release -IncludeReferencedProjects
 .\nuget.exe push ".\H.Versioning.*.nupkg" 79ccf130-386b-41b8-a8e2-6ce17f0459ca -Source https://www.nuget.org/api/v2/package
+
+call git checkout HEAD -- "H.Versioning.Cli\H.Versioning.Cli.nuspec"
