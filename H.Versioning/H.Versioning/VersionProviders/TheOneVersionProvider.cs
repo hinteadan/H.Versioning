bd020dcdb38cade069be6df9008f5c918cb6f6fa
Bogdan <bogdan.galiceanu@recognos.ro>
2017-03-02 11:37:10 +0200

    Read version file path from config if available

--- H.Versioning/H.Versioning/VersionProviders/TheOneVersionProvider.cs
+++ H.Versioning/H.Versioning/VersionProviders/TheOneVersionProvider.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
 
 namespace H.Versioning.VersionProviders
 {
     internal sealed class TheOneVersionProvider : VersionProviderPipeline
     {
+        private static readonly string versionFile = ConfigurationManager.AppSettings["H.Versioning.VersionFile"] ?? "version.txt";
+
         public TheOneVersionProvider()
             : base(
-                  Pipe(() => File.Exists("version.txt"), new FileVersionProvider("version.txt")),
+                  Pipe(() => File.Exists(versionFile), new FileVersionProvider(versionFile)),
                   Pipe(() => true, new GitVersionProvider(AppDomain.CurrentDomain.BaseDirectory))
                   )
         { }
